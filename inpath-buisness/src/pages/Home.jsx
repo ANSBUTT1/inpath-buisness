430834597ea685337ad11854dfed4b176407bf89
christopher <robbinc042@gmail.com>
2025-01-21 15:05:38 -0800

    resolve

--- inpath-buisness/src/pages/Home.jsx
+++ inpath-buisness/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import Header from '../components/include/Header'
-
+import BusinessPlatform from '../components/landingPage/BusinessPlatform'
+import OrganizationLearningImpact from '../components/landingPage/OrganizationLearningImpact'
+import CustomerExperience from '../components/landingPage/CustomerExperience'
+import MainCard from "../components/landingPage/MainCard"
+import BrandCarousel from "../components/landingPage/BrandCarousel"
+import ServiceCard from '../components/landingPage/ServiceCard'
+import HeroSection from '../components/landingPage/HeroSection'
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <div className='overflow-hidden'>
+      <HeroSection/>
+      <BrandCarousel/>
+      <ServiceCard/>
+      <MainCard/>
+     <BusinessPlatform/>
+      <CustomerExperience/>
+     <OrganizationLearningImpact/>
     </div>
   )
 }
