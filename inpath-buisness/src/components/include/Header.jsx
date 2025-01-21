4fd275fe319f3dc35cbd0222f3ad992306a55d24
christopher <robbinc042@gmail.com>
2025-01-21 09:04:23 -0800

    structure

---
+++ inpath-buisness/src/components/include/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+const Header = () => {
+  return (
+    <div className='px-40 p-4'>
+      <div className=''>
+        <img src={logo} alt="" className='' style={{ width: '8%' }} />
+        <div className=''>
+            <a href="">What we do</a>
+            <a href="">How we do it</a>
+            <a href="">Resources</a>
+            <a href="">Plans</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
